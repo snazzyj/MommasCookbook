3b6afa23b0747e7549b90fffe56be2dedbc9d529
AJ Olson <silentx.alex@gmail.com>
2020-05-01 15:17:02 -0400

    added recipe data render from user obj

--- src/profile/profile.js
+++ src/profile/profile.js
@@ -1,16 +1,26 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
-import Recipe from '../recipe/recipe';
+import CbCkContext from '../ckbkcontext';
 
 class Profile extends Component {
+
+    static contextType = CbCkContext;
+
     render() {
+        const {recipeData} = this.context.user;
         return (
             <Fragment>
-                <h1>Hello from dashboard</h1>
+                <h1>Welcome {this.context.user.firstName}</h1>
                 <Link to="/addrecipe">
                     Add Recipe
                 </Link>
-                <Recipe />
+
+                {recipeData.map((recipe) => {
+                    let url = `/recipe/${recipe.id}`
+                    return <li key={recipe.id}>
+                                <Link to={url}>{recipe.recipeName}</Link>
+                           </li>
+                })}
             </Fragment>
         )
     }
