4284baa3d85b685e2577ff64c8c47d8c8f7a98c3
AJ Olson <silentx.alex@gmail.com>
2020-05-01 15:46:45 -0400

    added login component

--- src/login/login.js
+++ src/login/login.js
@@ -1,9 +1,53 @@
 import React, {Component} from 'react';
+import AuthApiService from '../services/auth-api-service';
+import CkBkContext from '../ckbkcontext';
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: ''
+        };
+    }
+    static contextType = CkBkContext;
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+
+        const { email, password } = e.target;
+
+        AuthApiService.postLogin({
+            email: email.value,
+            password: password.value
+        })
+            .then(res => {
+                const { user } = res;
+                this.context.setUserLogin(user)
+                this.props.history.push('/')
+            })
+            .catch(error => {
+                this.setState({
+                    error
+                })
+            });
+
+    }
     render() {
         return (
-            <h1>Hello from Login</h1>
+            <section>
+                <h1>Login</h1>
+
+                <form onSubmit={this.handleLogin}>
+                    <label htmlFor="email">Email
+                        <input name="email" type="email" required/>
+                    </label>
+                    <label htmlFor="password">Password
+                        <input name="password" type="text" required />
+                    </label>
+                    <button>Login</button>
+                </form>
+            </section>
         )
     }
 }
