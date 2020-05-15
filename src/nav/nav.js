0829c712b51173958fd31e5483b539e4460e9391
AJ Olson <silentx.alex@gmail.com>
2020-05-15 15:54:42 -0400

    removed searchbox, ended up adding it back

--- src/nav/nav.js
+++ src/nav/nav.js
@@ -1,5 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
+import SearchBox from '../search/search';
 import CkBkContext from '../ckbkcontext';
 
 class Nav extends Component {
@@ -19,6 +20,7 @@ class Nav extends Component {
             {isLoggedIn && (
                 <Link to="/" onClick={this.handleLogout}>Logout</Link>
             )}
+            <SearchBox history={this.props.history} />
             </Fragment>
 
         )
