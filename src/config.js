b295f4a20f9eab1608b537a9853023b4ea879ab7
AJ Olson <silentx.alex@gmail.com>
2020-08-19 15:40:12 -0400

    added api to end of url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://serene-inlet-86243.herokuapp.com/',
+    API_ENDPOINT: 'https://serene-inlet-86243.herokuapp.com/api/',
     TOKEN_KEY: 'cookbook-special-secret'
 }
